1cf3717da9299979852eb9d10c60f2f4e39f6c0b
Keshav Kumar <keshavgujj99@gmail.com>
2024-11-09 17:37:25 +0530

    Day-04: ExecutorService practice

---
+++ src/multithreading/code/executorservice/practice1/ThreadPoolMain.java
@@ -0,0 +1,30 @@
+package multithreading.code.executorservice.practice1;
+
+import java.util.concurrent.*;
+
+public class ThreadPoolMain {
+    public static void main(String[] args) {
+
+        ExecutorService fixedThreadPool = Executors.newFixedThreadPool(3);
+        /*Executor cachedThreadPool =
+                new ThreadPoolExecutor(3,
+                        6,
+                        1000,
+                        TimeUnit.MILLISECONDS,
+                        new ArrayBlockingQueue<>(16));
+        ScheduledExecutorService scheduledThreadPool = new ScheduledThreadPoolExecutor(3);
+        ExecutorService singleThreadPool = Executors.newSingleThreadExecutor();*/
+
+        //submit the task to the pool to execute them
+        for(int i=1;i<=10;i++){
+            fixedThreadPool.execute(getRunnable("Task: " + i));
+        }
+        fixedThreadPool.shutdown();
+
+    }
+    public static Runnable getRunnable(String msg){
+        return ()->{
+            System.out.println(Thread.currentThread().getName() + ": " + msg);
+        };
+    }
+}

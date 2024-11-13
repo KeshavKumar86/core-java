db257285ba2719006ee1f68c74b0dfb81ac32e6b
Keshav Kumar <keshavgujj99@gmail.com>
2024-11-13 08:35:30 +0530

    Day-08: flatMap practice with meeting room example(not optimal)

--- src/gfg/problemoftheday/MeetingRooms.java
+++ src/gfg/problemoftheday/MeetingRooms.java
@@ -12,13 +12,25 @@ public class MeetingRooms {
                 {59, 105},
                 {23, 109}
         };
-        System.out.println(canAttend(arr));
+        //System.out.println(canAttend(arr));
+        System.out.println(canAttend2(arr));
     }
 
     private static boolean canAttend(int[][] arr) {
         Arrays.sort(arr, Comparator.comparingInt(a -> a[0]));
-        for(int i=0;i< arr.length-1;i++){
-            if(arr[i][1]>arr[i+1][0])
+        for (int i = 0; i < arr.length - 1; i++) {
+            if (arr[i][1] > arr[i + 1][0])
+                return false;
+        }
+        return true;
+    }
+
+    private static boolean canAttend2(int[][] arr) {
+        Arrays.sort(arr, Comparator.comparingInt(a -> a[0]));
+        int[] flatArr = Arrays.stream(arr).flatMapToInt((int[] array) ->
+                Arrays.stream(array)).toArray();
+        for (int i = 1; i + 1 < flatArr.length; i = i + 2) {
+            if (flatArr[i] > flatArr[i + 1])
                 return false;
         }
         return true;

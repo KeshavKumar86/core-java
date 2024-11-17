a5e0de13fa5c94fa3154c8947cb3f0d546b1b68b
Keshav Kumar <keshavgujj99@gmail.com>
2024-11-17 11:51:04 +0530

    Day-12: Single Responsibility Principle

---
+++ src/solid/srp/BankService.java
@@ -0,0 +1,14 @@
+package solid.srp;
+
+public class BankService {
+    public long deposit(long amount, String accountNumber){
+        //deposit amount
+        return 0;
+    }
+    public long withdraw(long amount, String accountNumber){
+        return 0;
+    }
+
+
+
+}
---
+++ src/solid/srp/LoanService.java
@@ -0,0 +1,15 @@
+package solid.srp;
+
+public class LoanService {
+    public void getLoanInterestInfo(String loanType){
+        if(loanType.equals("homeloan")){
+            //do some job
+        }
+        else if(loanType.equals("personalLoan")){
+            //do some job
+        }
+        else if(loanType.equals("carloan")){
+            //do some job
+        }
+    }
+}

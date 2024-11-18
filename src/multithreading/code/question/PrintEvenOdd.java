c0c68b2c7080d811985e5a4358c55896a7bdda0e
Keshav Kumar <keshavgujj99@gmail.com>
2024-11-18 09:21:13 +0530

    Day-13: Print Even Odd Using 2 Threads in Synchronized Manner

--- src/multithreading/code/question/PrintEvenOdd.java
+++ src/multithreading/code/question/PrintEvenOdd.java
@@ -1,31 +1,38 @@
 package multithreading.code.question;
 
 public class PrintEvenOdd {
-    private static final MonitorObject monitorObject = new MonitorObject();
     private static int i = 0;
+    private static final Object obj = new Object();
 
     public static void main(String[] args) {
         System.out.println("main thread starting");
         Thread evenThread = new Thread(() -> {
             while (i <= 10) {
-                System.out.println(i);
-                i++;
-                monitorObject.doNotify();
-                monitorObject.doWait();
+                synchronized (obj) {
+                    if (i <= 10 && i % 2 == 0) {
+                        System.out.println(i);
+                        i++;
+                        try {
+                            obj.wait();
+                        } catch (InterruptedException e) {
+                            throw new RuntimeException(e);
+                        }
+                    }
+                }
 
             }
-        });
+        }, "even");
         Thread oddThread = new Thread(() -> {
             while (i <= 10) {
-                monitorObject.doWait();
-                if (i <= 10) {
-                    System.out.println(i);
+                synchronized (obj) {
+                    if (i <= 10 && i % 2 != 0) {
+                        System.out.println(i);
+                        i++;
+                        obj.notify();
+                    }
                 }
-                i++;
-                monitorObject.doNotify();
-
             }
-        });
+        }, "odd");
 
         evenThread.start();
         oddThread.start();

8d6be9c0a2e6bdd9e51886ff81d8761b51c8b422
Keshav Kumar <keshavgujj99@gmail.com>
2024-11-22 22:08:26 +0530

    Day-16 StockBuyAndSell Time Complexity: O(n) Auxiliary Space: O(1) Maxima Minima Solution

--- src/gfg/problemoftheday/StockBuyAndSell.java
+++ src/gfg/problemoftheday/StockBuyAndSell.java
@@ -2,18 +2,30 @@ package gfg.problemoftheday;
 
 public class StockBuyAndSell {
     public static void main(String[] args) {
-
-        int[] prices = {100, 180, 260, 310, 40, 535, 695};
+        int[] prices = {4,3,2,1};
         System.out.println("Maximum Profit: " + maximumProfit(prices));
     }
 
     public static int maximumProfit(int[] prices) {
         // code here
-        int profit = 0;
-        for (int i = 1; i < prices.length; i++) {
-            if (prices[i] > prices[i - 1]) {
-                profit += prices[i] - prices[i - 1];
+        //Solution using pure local minima and pure local maxima approach
+        // //buy at local minima and sell at local maxima
+        int i = 0, buy , sale , profit = 0;
+        int n = prices.length;
+        while (i < n - 1) {
+            //find next local minima
+            while (i < n - 1 && prices[i] >= prices[i + 1]) {
+                i++;
             }
+            buy = i;
+            i = i+1;
+            if(i == n) break;
+            //find next local maxima
+            while (i < n - 1 && prices[i] <= prices[i + 1]) {
+                i++;
+            }
+            sale = i;
+            profit += prices[sale] - prices[buy];
         }
         return profit;
     }

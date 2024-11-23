c9d6236ad94cf1fc7cec27def3f43048eb20f64b
Keshav Kumar <keshavgujj99@gmail.com>
2024-11-23 20:52:03 +0530

    Day-18 MinimizeTheHeightsI Time Complexity: O(nlogn) Auxiliary Space: O(1)

---
+++ src/gfg/problemoftheday/MinimizeTheHeightsI.java
@@ -0,0 +1,26 @@
+package gfg.problemoftheday;
+
+import java.util.Arrays;
+
+public class MinimizeTheHeightsI {
+    public static void main(String[] args) {
+        int[] arr = {3, 9, 12, 16, 20};
+        int k = 3;
+        System.out.println(getMinDiff(k, arr));
+    }
+
+    public static int getMinDiff(int k, int[] arr) {
+        // code here
+        int n = arr.length;
+        Arrays.sort(arr);
+        int minDiff = arr[n - 1] - arr[0];
+        for (int i = 0; i <= n - 2; i++) {
+            int max = Math.max(arr[i] + k, arr[n - 1] - k);
+            int min = Math.min(arr[0] + k, arr[i + 1] - k);
+            minDiff = Math.min(minDiff, max - min);
+        }
+        return minDiff;
+    }
+
+}
+

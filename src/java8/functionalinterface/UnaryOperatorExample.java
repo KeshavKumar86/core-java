7e2fd59a0ca6079c1bcfe3db695afd420f51cb7d
Keshav Kumar <keshavgujj99@gmail.com>
2024-12-08 11:18:57 +0530

    Day-33 UnaryOperator FunctionalInterface

---
+++ src/java8/functionalinterface/UnaryOperatorExample.java
@@ -0,0 +1,18 @@
+package java8.functionalinterface;
+
+import java.util.function.UnaryOperator;
+
+public class UnaryOperatorExample {
+    public static void main(String[] args) {
+
+        /* For UnaryOperator return type and parameter type
+        of apply function will be same.
+        when return type and parameter type
+        of apply function is same than we can use UnaryOperator instead of Function
+         */
+        UnaryOperator<Integer> function = (Integer i) -> i * 4;
+        System.out.println("Result: " + function.apply(16));
+
+
+    }
+}
